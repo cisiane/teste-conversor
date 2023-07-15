3e2fa5963ff8f3218105628b3a90f797fabf77d4
cisiane <cisiane@hotmail.com>
2023-07-15 18:09:28 -0300

    Commit initial

---
+++ teste_conversor/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author cisia
+ *
+ */
+module TesteConversor {
+}
---
+++ teste_conversor/src/teste_conversor/ConversorApp.java
@@ -0,0 +1,12 @@
+package teste_conversor;
+
+
+public class ConversorApp {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner();
+
+	}
+
+}
